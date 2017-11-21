e57c985389e0849c4b229348c30107d67c8db8cb
root <root@vclv13-141.hpc.ncsu.edu>
2017-11-21 15:43:44 -0500

    struct updated

--- src/nphfuse_extra.h
+++ src/nphfuse_extra.h
@@ -7,6 +7,34 @@
   You may extend this file if necessary  
 */
 
+
+#include <sys/stat.h>
+
+#define SUPER_BLOCK_SIZE 1
+#define SUPER_BLOCK_MIN 0
+#define SUPER_BLOCK_MAX (SUPER_BLOCK_MIN+SUPER_BLOCK_SIZE)
+
+#define INODE_BLOCK_SIZE 10
+#define INODE_BLOCK_MIN (SUPER_BLOCK_MAX+1)
+#define INODE_BLOCK_MAX (INODE_BLOCK_MIN+INODE_BLOCK_SIZE)
+
+#define DATA_BLOCK_SIZE 16000
+#define DATA_BLOCK_MIN (INODE_BLOCK_MAX+1)
+#define DATA_BLOCK_MAX (DATA_BLOCK_MIN+DATA_BLOCK_SIZE)
+
+#define MAX_NAME 50
+#define DATA_BLOCK_SIZE 8192
+
+typdef struct {
+	struct  stat fstat;
+	char 		 fileName[MAX_NAME];
+	char		 dirName[MAX_NAME];
+	char		 pad[2];	
+} tInodeInfo;
+
+#define BLOCK_ENTRIES_NUM (BLOCK_SIZE/sizeof(tInodeInfo))
+
+
 struct nphfuse_state {
     FILE *logfile;
     char *device_name;
