4d57d80ea2189b760aed79428ef1779ca3a5b539
rishabhprathore <rrathor@ncsu.edu>
2017-11-30 20:25:51 -0500

    test

--- src/nphfuse_extra.h
+++ src/nphfuse_extra.h
@@ -7,36 +7,34 @@
   You may extend this file if necessary  
 */
 
-
 #include <sys/stat.h>
 #include <stdint.h>
 
 #define SUPER_BLOCK_COUNT 1
-#define SUPER_BLOCK_START 0
-#define SUPER_BLOCK_END 1
+#define SUPER_BLOCK_START 1
+#define SUPER_BLOCK_END 2
 
-#define INODE_BLOCK_COUNT 50
-#define INODE_BLOCK_START 1
-#define INODE_BLOCK_END 51
+#define INODE_BLOCK_COUNT 998
+#define INODE_BLOCK_START 2
+#define INODE_BLOCK_END 1000
 
 #define DATA_BLOCK_COUNT 10000
-#define DATA_BLOCK_START 51
-#define DATA_BLOCK_END 10051
+#define DATA_BLOCK_START 1000
+#define DATA_BLOCK_END 11000
 
-#define FILE_MAX 32
+#define FILE_MAX 128
 #define BLOCK_CAPACITY 8192
-#define DIR_MAX 64
+#define DIR_MAX 224
 
-typedef struct {
+typedef struct i_node{
 	struct  stat fstat;
 	char 		 file_name[FILE_MAX];
-  char		 dir_name[FILE_MAX];
+  char		 dir_name[DIR_MAX];
   uint64_t offset;
   long int		 pad;	
 } i_node;
 
-#define INODE_NUM (BLOCK_CAPACITY/sizeof(i_node))
-
+#define INODE_NUM (BLOCK_CAPACITY/sizeof(i_node)) //16
 
 struct nphfuse_state {
     FILE *logfile;
