ca162d5b21f2834bf8811b14ec56cafb9923b3f4
rishabhprathore <rrathor@ncsu.edu>
2017-12-01 21:31:27 -0500

    test

--- src/nphfuse_extra.h
+++ src/nphfuse_extra.h
@@ -10,20 +10,7 @@
 #include <sys/stat.h>
 #include <stdint.h>
 
-#define SUPER_BLOCK_COUNT 1
-#define SUPER_BLOCK_START 1
-#define SUPER_BLOCK_END 2
-
-#define INODE_BLOCK_COUNT 998
-#define INODE_BLOCK_START 2
-#define INODE_BLOCK_END 1000
-
-#define DATA_BLOCK_COUNT 10000
-#define DATA_BLOCK_START 1000
-#define DATA_BLOCK_END 11000
-
 #define FILE_MAX 128
-#define BLOCK_CAPACITY 8192
 #define DIR_MAX 224
 
 typedef struct i_node{
@@ -33,8 +20,6 @@ typedef struct i_node{
   char		 dir_name[DIR_MAX];
   long int		 pad;	
 } i_node;
-
-#define INODE_NUM (BLOCK_CAPACITY/sizeof(i_node)) //16
 
 struct nphfuse_state {
     FILE *logfile;
